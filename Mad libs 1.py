7146fa8d7c47f3bc3163252d9d448f5d8a41103b
khaledalseiari <59317326+khaledalseiari@users.noreply.github.com>
2021-06-25 18:59:45 +0400

    Add files via upload

---
+++ Mad libs 1.py
@@ -0,0 +1,11 @@
+print("WELCOME TO THE MAD LIBS STORY THING")
+
+adjective1 = str(input("Adjective "))
+location1 = str(input("Location "))
+animal1 = str(input("Animal "))
+verb1 = str(input("Verb (past tense) "))
+
+print("STORYTIME")
+
+print("One day a boy was walking through the dark", adjective1, "streets of", location1)
+print("When suddenly a", animal1, "got in his way and he", verb1, "it")
